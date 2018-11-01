2257a7fb480f61c68fa4e4e56c3ecfa2e0a110bf
Kam Louis <mrklouis11@gmail.com>
2018-11-01 20:33:51 +0100

    Clean up and a dd useful comments.

--- server/server.js
+++ server/server.js
@@ -1,22 +1,27 @@
+/* eslint-disable no-console */
+
+// Load env config
+require('dotenv').config();
+
+// Require npm modules
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-// Get env config
-require('dotenv').config();
-const config = require('../config/config')();
-
-// Set routes
+// Load routes
 const users = require('./routes/api/user');
 
+// Initialize app
 const app = express();
-const { PORT = 3000 } = process.env;
-const db = config.mongo.uri;
+const { PORT = 5000, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const db = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
+// Initialize middleware
 app.use(helmet());
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors());
 app.use(express.static('./dist/'));
 
@@ -26,10 +31,11 @@ mongoose
     db,
     { useNewUrlParser: true }
   )
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(`${err}`));
+  .then(() => console.log('Connected Successfully to MongoDB'))
+  .catch(err => console.log(err));
 
 // Use routes
+
 app.use('/api/users', users);
 
 app.get('/api', (req, res) => {
@@ -38,6 +44,7 @@ app.get('/api', (req, res) => {
   });
 });
 
+// Listen on the designated port
 app.listen(PORT, err => {
   if (err) console.log(err);
   console.info(`==> Listening on port ${PORT}`);
